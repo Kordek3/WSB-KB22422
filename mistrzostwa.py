dc55a34d52c29bada732671063064b32ee49d0da
Kordek3 <konradbieniak@outlook.com>
2023-06-23 17:05:41 +0200

    Revert "Test gitignore"
    This reverts commit 6f11b9e65ae9c9aa5c588288589dd063e9c6d775.

--- mistrzostwa.py
+++ mistrzostwa.py
@@ -5,7 +5,6 @@ data = {
     'Rok': [2002, 2006, 2010],
     'Zwycięzca': ['Brazylia', 'Włochy', 'Hiszpania'],
     'Król strzelców': ['Ronaldo (8 goli)', 'Klose (5 goli)', 'Forlan (5 goli)'],
-    'Cena biletu': ['100 USD', '120 USD', '150 USD'],
 }
 
 # Tworzenie tabeli
